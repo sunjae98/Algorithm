2d843bbe63ea1422d9481b0cbaadac570e1b2b3a
Sunjae Kim <83015089+sunjae98@users.noreply.github.com>
2024-03-31 17:38:38 +0900

    [Gold V] Title: 토마토, Time: 2236 ms, Memory: 105784 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,64 +1,52 @@
+'''
+[BFS]
+1. 2차원 배열 visited 를 생성해서 0으로 초기화
+2. BFS -> 방문시 visited[i][j] 값 증가
+3. 모두 익을 때까지의 최소 날짜 = visited 테이블에서 최대값
+'''
 import sys
 from collections import deque
 
-# 상우하좌
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
+col, row = map(int, sys.stdin.readline().strip().split())
+maps = []
+for _ in range(row):
+    maps.append(list(map(int, sys.stdin.readline().strip().split())))
 
-# M, N 입력
-M, N= map(int, sys.stdin.readline().strip().split())
+# 이동할 네 가지 방향 (상, 하, 좌, 우)
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
-tray = []
-
-# 토마토 칸
-for _ in range(N):
-  tray.append(list(map(int, sys.stdin.readline().strip().split())))
-
-Q = deque()
+q = deque()
 
 # 토마토 중 익은 토마토들을 Q에 삽입
-for r in range(N):
-  for c in range(M):
-    if tray[r][c] == 1:
-      Q.append([r, c])
-
-# 익은 토마토가 더이상 없을 때까지 반복
-while Q:
-  # 현재 큐에 삽입되어 있는 익은 토마토 개수만큼 반복
-  length = len(Q)
-  for _ in range(length):
-    # 익은 토마토 하나 꺼내기
-    r, c = Q.popleft()
-
-    # 익은 토마토의 인접한 토마토들 비교
-    for i in range(4):
-      nr = r + dr[i]
-      nc = c + dc[i]
-
-      # 토마토 칸의 범위에서 벗어나지 않거나
-      # 익지 않은 토마토인 경우에만
-      if nr >= 0 and nr < N and nc >= 0 and nc < M and tray[nr][nc] == 0:
-        # 현재 칸까지의 소요일
-        # 1로 시작했으므로 마지막에 -1 해야함
-        tray[nr][nc] = tray[r][c] + 1
+for r in range(row):
+    for c in range(col):
+        if maps[r][c] == 1:
+            q.append([r, c])
         
-        # 이제 익은 토마토이므로 큐에 삽입
-        Q.append([nr, nc])
-        
+while q:
+    # 현재 큐에 삽입되어 있는 익은 토마토 개수만큼 반복
+    length = len(q)
+    for _ in range(length):
+        # 익은 토마토 하나 꺼내기
+        r, c = q.popleft()
+            
+        # 익은 토마토의 네 방향으로 이동하며 탐색
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            # 1.범위 2.익지 않은 토마토
+            if 0 <= nr < row and 0 <= nc < col and maps[nr][nc] == 0:
+                q.append([nr, nc]) # 큐에 추가
+                maps[nr][nc] = maps[r][c] + 1
+
 day = 0
+for row in maps:
+    for tomato in row:
+    # BFS 이후에 안익은 토마토가 존재
+        if tomato == 0:
+            print(-1)
+            exit(0)
+    day = max(day, max(row))
 
-# 토마토 칸의 행
-for row in tray:
-  # 토마토 칸의 행의 토마토들
-  for tomato in row:
-    # BFS를 돌았는데도 익지 않은 토마토가 존재하냐
-    if tomato == 0:
-      # 그렇다면 모두 익지는 못하는 상황이므로 -1
-      print(-1)
-      # 그리고 종료
-      exit(0)
-  # 토마토들 중 가장 큰 값(소요일)을 찾는다
-  day = max(day, max(row))
-
-# 익은 토마토(1)부터 +1 했으므로 최종적으로 -1
+# 익은토마토부터 시작했으므로 -1
 print(day - 1)
